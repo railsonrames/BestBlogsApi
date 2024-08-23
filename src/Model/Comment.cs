41da3631d14ffe627585636ae64cc50dcb106939
Devskiller <owl@devskiller.com>
2024-08-23 16:43:35 +0000

    Candidate repo initialization

---
+++ src/Model/Comment.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Model
+{
+    public record Comment
+    {
+        public Guid Id { get; set; }
+        public Guid PostId { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ src/Model/Post.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Model
+{
+    public record Post
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
